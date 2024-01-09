c503f76a7294cecd17582d76f40723e6778f61d1
nadeemcite <nady5730642@gmail.com>
2024-01-09 14:07:41 +0530

    fix: frontend and ml

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,8 +82,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME", default="your_db_name"),
+        "USER": config("DB_USERNAME", default="your_db_user"),
+        "PASSWORD": config("DB_PASSWORD", default="your_db_password"),
+        "HOST": config("DB_HOST", default="your_db_password"),
+        "PORT": config("DB_PORT", default="your_db_password", cast=int),
     }
 }
 
